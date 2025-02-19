ab0fcbcdd369835e1a1688407887c69b71eac91a
Javasession2024 <javasession123@gmail.com>
2025-02-19 19:55:31 +0530

    Git Testing

--- src/main/java/practice/Test.java
+++ src/main/java/practice/Test.java
@@ -7,7 +7,23 @@ public class Test {
     public static void main(String[] args) {
 
 
+        String str = "Hello";
+        String str1 = str + "World";
 
+        System.out.println("Hash Code 0 "+str.hashCode());
+
+        System.out.println("Hash Code 1 "+str1.hashCode());
+
+        String str2 = "Helloworld";
+        System.out.println("Hash Code 2 "+str2.hashCode());
+
+        String str3 = str2.intern();
+        System.out.println("Hash Code 3 "+str3.hashCode());
+
+        if(str1.equals(str3))
+            System.out.println("Equal");
+        else
+            System.out.println("Not equal");
 
 
 
